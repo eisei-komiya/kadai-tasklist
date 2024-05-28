7d0de39c7ca3b6171a008c822977d14ea4a3eaa2
eisei.komiya <eisei-komiya@dac.co.jp>
2024-05-28 04:24:46 +0000

    kadai -tasklist

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('tasks', TasksController::class);
+Route::get('/', [TasksController::class, 'index']);
